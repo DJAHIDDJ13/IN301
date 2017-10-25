f7198c9c3ce7887db8f690049b54429894a6d9a0
Utilisateur <user@debian.arcahe.ovh>
2017-10-25 09:42:51 +0200

    continuation de Listes chainees

--- ExoLangC/lisChai/list.c
+++ ExoLangC/lisChai/list.c
@@ -25,7 +25,7 @@ void affListe(Liste *l){
 	printf("\n");
 }
 
-void pop(Liste *l){
+void popLast(Liste *l){
 		Liste *cur = l;
 		while(cur->suiv->suiv){
 			cur = cur->suiv;
@@ -34,6 +34,9 @@ void pop(Liste *l){
 		cur->suiv = NULL;
 }
 
+void popHead(Liste *l){
+	l = l->suiv;
+}
 Liste *pushFirst(Liste *l, int val){
 	Liste *m = malloc(sizeof(Liste));
 	m->val = val;
@@ -68,16 +71,51 @@ _Bool estTri(Liste *l){
 	return 1;
 }
 
+int nbrEle(Liste *l){
+	int cmpt = 1;
+	while(l->suiv){
+		cmpt++;
+		l=l->suiv;
+	}
+	return cmpt;
+}
+
+int recEle(Liste *l, int e){
+	int i = 0;
+	while(l->suiv){
+		if(e == l->val)
+			return i;
+		l=l->suiv;
+		i++;
+	}
+	return -1;
+}
+
+int supEle(Liste *l, int e){
+	int rec = recEle(l,e);
+	if(rec==-1) return 0;
+	else if(rec==0)popHead(l);
+	for(int i=0; i<rec-1; i++)
+		l = l->suiv;
+	l->suiv = l->suiv->suiv;
+	return 0;
+}
 int main(void){
 	srand (time(NULL));
 	Liste *l = creerListeVide();
-	for(int i=0; i<2; i++)
-		l = pushLast(l,alea(5));
+	//for(int i=0; i<5; i++) l = pushLast(l,alea(5));
+	l=pushLast(pushLast(l,0),1);
 	affListe(l);
-
-	if(estTri(l))
-		printf("\nl est triee\n");
-	else
-		printf("\nl n'est pas triee\n");
+	printf("%s",(estTri(l))?"\nl est triee\n":"\nl n'est pas triee\n");
+	
+	printf("\nLe nombre d'elements: %d\n\n", nbrEle(l));
+	
+	int al = 1;
+	printf("Recherche de %d: %d\n\n", al, recEle(l, al));
+	
+	printf("Supression de cet element.\n");
+	supEle(l,al);
+	affListe(l);
+	printf("\n");
 	return 0;
 }
