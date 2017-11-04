bce2c759c4bbb186d97a923f906bd4a5e489b65b
Djahid <Djahidabdelmoumen@gmail.com>
2017-11-04 19:06:55 +0100

    progress on cube projection

--- LANGAGE_C/Tests/cubeProjection/cube.c
+++ LANGAGE_C/Tests/cubeProjection/cube.c
@@ -2,13 +2,60 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#define PI 3.1415
 typedef struct pt3d {
 	double x,y,z;
 } pt3d;
 
 typedef struct cube{
-	pt3d a[8];
+	pt3d vertex[8];
 } cube;
+
+pt3d rotate(pt3d start, char axe, float theta);
+
+int main(void){
+	cube c;
+	int index = 0;
+	for(int i=0; i<2; i++){
+		for(int j=0; j<2; j++){
+			for(int k=0; k<2; k++){
+				c.vertex[index].x = (double)i;
+				c.vertex[index].y = (double)j;
+				c.vertex[index].z = (double)k;
+				index ++;
+			}
+		}
+	}
+
+	POINT n[8];
+	for(int i=0; i<8; i++){
+		n[i].x = c.vertex[i].x * 100+100;
+		n[i].y = c.vertex[i].y * 100+100;
+	}
+	init_graphics(300, 300);
+	fill_screen(blanc);
+
+	while(1){		
+		for(int i=0; i<8; i++){
+			c.vertex[i] = rotate(c.vertex[i], 'x', 0.01);
+		}
+		for(int i=0; i<8; i++){
+			c.vertex[i] = rotate(c.vertex[i], 'y', 0.01);
+		}
+		for(int i=0; i<8; i++){
+			n[i].x = c.vertex[i].x * 100+100;
+			n[i].y = c.vertex[i].y * 100+100;
+		}
+		int lines[12][2] = {{0,1},{0,2},{0,4},{1,3},{1,5},{2,3},{2,6},{3,7},{4,5},{4,6},{5,7},{6,7}};
+		for(int i=0; i<12; i++){
+			draw_line(n[lines[i][0]], n[lines[i][1]], noir);
+		}
+		attendre(10);
+		fill_screen(blanc);
+	}
+	return 0;
+}
+
 
 pt3d rotate(pt3d start, char axe, float theta){
 	pt3d end;
@@ -28,21 +75,4 @@ pt3d rotate(pt3d start, char axe, float theta){
 		end.z = start.z;
 	}
 	return end; 
-}
-int main(void){
-	cube c;
-	int index = 0;
-	for(int i=0; i<2; i++){
-		for(int j=0; j<2; j++){
-			for(int k=0; k<2; k++){
-				c.a[index].x = (double)i;
-				c.a[index].y = (double)j;
-				c.a[index].z = (double)k;
-				index ++;
-				printf("%d,%d,%d\n",i,j,k);
-			}
-		}
-	}
-	
-	return 0;
 }
