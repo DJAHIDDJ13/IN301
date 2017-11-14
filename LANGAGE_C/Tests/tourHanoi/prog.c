972d06a5cf3fb83f4dcd701ae467e21f75cf6555
Djahid <Djahidabdelmoumen@gmail.com>
2017-11-14 18:01:45 +0100

    Solution de tours de hanoi en C

--- LANGAGE_C/Tests/tourHanoi/prog.c
+++ LANGAGE_C/Tests/tourHanoi/prog.c
@@ -5,7 +5,9 @@ typedef struct liste{
 	int val;
 	struct liste* suiv;
 } liste;
-
+typedef struct{
+	liste* l[3];
+} han;
 int hd(liste* l){
 	if(l==NULL)
 		exit(EXIT_FAILURE);
@@ -41,31 +43,76 @@ void affichePile(liste* p){
 		printf("n\n");
 		return;
 	}
+	int tab[100];
+	int i=0;
 	while(p){
-		printf("%d ", p->val);
+		tab[i] = p->val;
+		i++;
 		p = p->suiv;
 	}
+	for(int j=i-1; j>=0; j--)
+		printf("%d ", tab[j]);
 	printf("\n");
 	
 }
 void affichePiles(liste* p[3]){
 	for(int i=0; i<3; i++)
 		affichePile(p[i]);
+	printf("\n");
 }
-passerEle(liste* p[3], int p, int g){
-	if(p<0||p>2  ||  g<0 || p>2){
-		fprintf("Unauthorized ")
+liste** passerEle(liste* p[3], int s, int g){
+	if(s<0||s>2  ||  g<0 || g>2){
+		fprintf(stderr,"Unauthorized access g or p\n");
+		exit(EXIT_FAILURE);
 	}
+	p[g] = insere(p[g], hd(p[s]));
+	p[s] = next(p[s]);
+	return p;
+}
+int other(int s, int g){
+	if(s == 0){
+		if(g == 1)
+			return 2;
+		else if(g == 2)
+			return 1;
+	} else if(s == 1){
+		if(g == 0)
+			return 2;
+		else if(g == 2)	
+			return 0;
+	} else if(s == 2){
+		if(g == 0)
+			return 1;
+		else if(g == 1)
+			return 0;
+	}
+	return -1;
+}
+han hanoi(han p, int s, int g, int n){
+	if(n == 0)
+		return p;
+	//printf("s = %d, g = %d, other()= %d\n",s,g,other(s,g));
+	p = hanoi(p, s, other(s,g), n-1);
+	affichePiles(p.l);
+	passerEle(p.l, s, g);
+	p = hanoi(p, other(s,g), g, n-1);
+	return p;
 }
 int main(int argc, char *argv[]){
-	
 	if(argc != 2 || atoi(argv[1]) == 0){
 		fprintf(stderr, "ARGUMENT ERROR\n");
 		exit(EXIT_FAILURE);
 	}
+	han p;
+	p.l[0] = creerListe();
+	p.l[1] = creerListe();
+	p.l[2] = creerListe();
 	int n = atoi(argv[1]);
-	liste* p[3] = {creerListe(),creerListe(),creerListe()};
-	p[0] = initPile(p[0], n);
-	affichePiles(p);
+
+	p.l[0] = initPile(p.l[0], n);
+	p = hanoi(p, 0, 2, n);
+	affichePiles(p.l);
+	clrscr();
+	getchar();
 	exit(EXIT_SUCCESS);
 }
