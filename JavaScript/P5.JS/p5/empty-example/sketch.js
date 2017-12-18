abfab70fdf89cce129ba836616ac6d0c70f07c23
Djahid <Djahidabdelmoumen@gmail.com>
2017-12-18 11:18:24 +0100

    js

--- JavaScript/P5.JS/p5/empty-example/sketch.js
+++ JavaScript/P5.JS/p5/empty-example/sketch.js
@@ -1,11 +1,22 @@
 function setup(){
-  createCanvas(400, 400, WEBGL);
+  createCanvas(400, 400);
 
 }
 
 function draw(){
   background(200);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(200, 200, 200);
+  branch(50);
+}
+
+function branch(len){
+  if(len < 2)
+    return;
+  stroke(0);
+  line(0,0,0,-len);
+  console.log(len);
+
+  translate(0,-len);
+  rotate(PI / 4);
+  branch(len*0.67);
+
 }
